a00239a84745d81d5bfb19623e7c4f5ef6c8eb17
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2018-04-02 20:01:51 -0400

    new animations to featured section

--- assets/script/portfolio.js
+++ assets/script/portfolio.js
@@ -74,12 +74,65 @@ $(document).ready(function(){
 	  	offset: "50%"
   });
 
-  $(".js-wp3").waypoint(function(direction){
+  //FEATURED
 
-		$(".js-wp3").addClass("animated fadeInRight");
+  $(".js-wp-1").waypoint(function(direction){
+
+		$(".js-wp-1").addClass("animated fadeInLeft");
 	  }, {
-	  	offset: "50%"
+	  	offset: "90%"
   });
+
+  $(".js-wp-2").waypoint(function(direction){
+
+		$(".js-wp-2").addClass("animated fadeInLeft");
+	  }, {
+	  	offset: "90%"
+  });
+
+  $(".js-wp-3").waypoint(function(direction){
+
+		$(".js-wp-3").addClass("animated fadeInLeft");
+	  }, {
+	  	offset: "90%"
+  });
+
+  $(".js-wp-4").waypoint(function(direction){
+
+		$(".js-wp-4").addClass("animated fadeInLeft");
+	  }, {
+	  	offset: "90%"
+  });
+
+  $(".js-wp-5").waypoint(function(direction){
+
+		$(".js-wp-5").addClass("animated fadeInRight");
+	  }, {
+	  	offset: "90%"
+  });
+
+  $(".js-wp-6").waypoint(function(direction){
+
+		$(".js-wp-6").addClass("animated fadeInRight");
+	  }, {
+	  	offset: "90%"
+  });
+
+  $(".js-wp-7").waypoint(function(direction){
+
+		$(".js-wp-7").addClass("animated fadeInRight");
+	  }, {
+	  	offset: "90%"
+  });
+
+  $(".js-wp-8").waypoint(function(direction){
+
+		$(".js-wp-8").addClass("animated fadeInRight");
+	  }, {
+	  	offset: "90%"
+  });
+
+
 
 
   //Toggle Panels

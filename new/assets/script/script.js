092c7570efc31deff6228400d09582840044994f
Kaitlyn Folsom <kaitlynmfolsom@gmail.com>
2018-08-22 22:22:20 -0400

    modal updates

--- new/assets/script/script.js
+++ new/assets/script/script.js
@@ -1,70 +1,218 @@
-
+var projectObj = [
+	{
+		projectName: "Dinner and A Show",
+		overview: "A web application that helps you discover highly rated, local restaurants based on the Broadway show you are attending in New York City. This site uses four API's to create an application for users to search for a broadway show (SeatGeek API) and a place to eat nearby (Yelp API). Additionally, you can get walking directions (Google Maps API) from the restaurant to the theatre and the current weather(Open Weather API).",
+		tech: [
+			"Javascript",
+			"JQuery",
+			"HTML",
+			"CSS",
+			"SeatGeek API",
+			"Open Weather API",
+			"Google Maps",
+			"Yelp API"
+		],
+		screenshots: [
+			"assets/images/dinner&show.png",
+			"assets/images/trivia.png",
+			"assets/images/friend-forum.png"
+		],
+		collaborators: [
+			"Kaitlyn Folsom",
+			"Louis Hellinger",
+			""
+		],
+		githubLink: "",
+		projectLink: ""
+	},
+	{
+		projectName: "RxMinder",
+		overview: "A React-based web application designed to schedule medication reminders for others. Gives you, as the caretaker or family member, peace of mind while your patient or loved one maintains their independence. Allows you to remain at ease knowing that they are following their regimen.",
+		tech: [
+			"React",
+			"JSX",
+			"Passport",
+			"Twillio",
+		],
+		screenshots: [
+			"assets/images/dinner&show.png",
+			"assets/images/trivia.png",
+			"assets/images/rxMinder.png"
+		],
+		collaborators: [
+			"Andrew Apicello",
+			"Kaitlyn Folsom",
+			"Nicole Carvalho",
+			"Annie Leote"
+		],
+		githubLink: "https://github.com/Jewel0106/RxMinder",
+		projectLink: "https://rx-minder.herokuapp.com/"
+	},
+	{
+		projectName: "Friend Forum",
+		overview: "A social media web application those who wish to have professional discussions with people around the country on topics you share interests in. Chat and debate through our live chat feature specific to each topic or send out a mesage in our global feed for all users to see.",
+		tech: [
+			"Javascript",
+			"JQuery",
+			"HTML",
+			"CSS",
+			"Bootstrap",
+			"Handlebars",
+			"MySql",
+			"Sequelize",
+			"Express",
+			"Socket.io",
+			"Moment",
+			"News API"
+		],
+		screenshots: [
+			"assets/images/dinner&show.png",
+			"assets/images/trivia.png",
+			"assets/images/friend-forum.png"
+		],
+		collaborators: [
+			"Andrew Apicello",
+			"Benjamin Danis",
+			"Kaitlyn Folsom",
+			"Alexander Paulino"
+		],
+		githubLink: "https://github.com/Jewel0106/Friend-Forum",
+		projectLink: "https://friend-forum-social.herokuapp.com"
+	},
+	{
+		projectName: "Wine Trivia",
+		overview: "This quiz on random wine facts has 10 multiple choice questions. You will have 30 seconds to answer each question. Good Luck!",
+		tech: [
+			"Javascript",
+			"JQuery",
+			"HTML",
+			"CSS"
+		],
+		screenshots: [
+			"assets/images/dinner&show.png",
+			"assets/images/trivia.png",
+			"assets/images/friend-forum.png"
+		],
+		collaborators: [],
+		githubLink: "https://github.com/Jewel0106/TriviaGame",
+		projectLink: "https://jewel0106.github.io/TriviaGame/"
+	},
+	{
+		projectName: "Knox Cuisine",
+		overview: "",
+		tech: [
+			"Wordpress",
+			"Javascript",
+			"CSS",
+		],
+		screenshots: [
+			"assets/images/dinner&show.png",
+			"assets/images/trivia.png",
+			"assets/images/friend-forum.png"
+		],
+		collaborators: [],
+		githubLink: "",
+		projectLink: "http://www.knoxcuisine.com"
+	}
+]
 
 $('document').ready(function(){
-	$('.carousel').carousel()
 
+	for (var i = 0; i < projectObj.length; i++) {
+		console.log(projectObj[i]);
+	}
 
-	console.log("Hello Jimmy")
-	// ===================
-	// == ANIMATED MODAL
-	// ===================
+	$(".close-animatedModal").click(function() {
+		$("#projectTitle").empty();
+
+		$("#project-overview").empty();
+
+		$(".tech-items").empty()
+
+		// $(".project-slideshow").empty();
+	});
+
+	$('.carousel').carousel();
+
+	// =======================
+	// == ANIMATED MODAL =====
+	// =======================
 
 	$("#modal1").animatedModal();
 	$("#modal1").click(function() {
-		getdinnerShowProject();
+		getProject(projectObj[0]);
 	});
 
 	$("#modal2").animatedModal();
 	$("#modal2").click(function() {
-		getRxProject();
+		getProject(projectObj[1]);
 	});
 
 	$("#modal3").animatedModal();
 	$("#modal3").click(function() {
-		getForumProject();
+		getProject(projectObj[2]);
 	});
 
 	$("#modal4").animatedModal();
 	$("#modal4").click(function() {
-		getTriviaProject();
+		getProject(projectObj[3]);
 	});
 
 	$("#modal5").animatedModal();
 	$("#modal5").click(function() {
-		getCuisineProject();
+		getProject(projectObj[4]);
 	});
 
-	function getdinnerShowProject() {
+
+	function getProject(project) {
+		console.log(project);
+
+		$("#projectTitle").text(project.projectName);
+		$("#project-overview").text(project.overview);
+
+		for (var i = 0; i < project.tech.length; i++) {
+			$(".tech-items").append("<li>" + project.tech[i] + "</li>")
+		}
+
+		for (var i = 0; i < project.screenshots.length; i++) {
+			console.log(project.screenshots[i])
+			$(".project-slideshow").append("<div><img src='" + project.screenshots[i] + "'></div>")
+		}
+
 		$('.project-slideshow').slick({
 			autoplay: true,
 			autoplaySpeed: 3000,
-			fade: true
-
+			fade: true,
+			arrows:false,
+			dots:true,
+			slidesToShow:1,
+			customPaging: function(slider, i) {
+				console.log(slider);
+				console.log(i);
+				$(".slideshow-dots").append("<img src='" + project.screenshots[i] + "'>");
+			}, 
+			infinite:true,
+			dotsClass: "image-pagers",
+			cssEase: 'linear',
+			focusOnSelect: true,
 		});
 
-		$("#projectTitle").text("Dinner & A Show");
+		for (var i = 0; i < project.collaborators.length; i++) {
+			$(".collaborators").append("<li>" + project.collaborators[i] + "</li>")
+		}
 
-		$("#project-overview").text("A React-based web application designed to schedule medication reminders for others. Gives you, as the caretaker or family member, peace of mind while your patient or loved one maintains their independence. Allows you to remain at ease knowing that they are following their regimen.");
+		if (project.githubLink != "") {
+			$(".links").append('<div class="project-links d-flex justify-content-center github-link"><a href="' + project.githubLink + '" class="shimmer">GITHUB REPO</a></div>')
+		}
+
+		if (project.projectLink != "") {
+			$(".links").append('<div class="project-links d-flex justify-content-center github-link"><a href="' + project.projectLink + '" class="shimmer">GITHUB REPO</a></div>')
+		}
 	}
 
-	function getForumProject() {
-		$("#projectTitle").text("Friend Forum");
-	}
-
-	function getRxProject() {
-		$("#projectTitle").text("RxMinder");
-	}
-
-	function getTriviaProject() {
-		$("#projectTitle").text("Wine Trivia");
-	}
-
-	function getCuisineProject() {
-		$("#projectTitle").text("Knox Cuisine");
-	}
-
-
-	// NAVIGATION
+	// ===================
+	// == NAVIGATION =====
+	// ===================
 	$('#nav-toggle').click(function() {
 		$(this).toggleClass('active');
 		$('#overlay').toggleClass('open');
